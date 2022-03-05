ae16329e54967078fba9a52beb85ab866ef538fe
William Yoon <89663919+wty5@users.noreply.github.com>
2022-03-05 15:27:10 -0500

    Completed lab 1.3

--- C1/week2/labB.py
+++ C1/week2/labB.py
@@ -1,29 +1,38 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow import keras
-
-class myCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs={}):
-        if(logs.get('accuracy') >= 0.6):
-
-            print("\nAccuracy is higher than 0.6 so stop training")
-            self.model.stop_training = True
-
-callbacks = myCallback()
-
-fmnist = tf.keras.datasets.fashion_mnist
-(x_train, y_train),(x_test, y_test) = fmnist.load_data()
-x_train = x_train / 255.0
-x_test = x_test / 255.0
-
+# Define the model
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(512, activation=tf.nn.relu),
-    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+
+  # Add convolutions and max pooling
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+
+
+  # Add the same layers as before
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-model.compile(optimizer="adam",
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+class myCallBack(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy') >= 0.9):
+      print("\nAccuracy is higher than 0.9 so stop training")
+      self.model.stop_training = True
 
-model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
+callbacks = myCallBack()
+
+# Print the model summary
+model.summary()
+
+# Use same settings
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+print(f'\nMODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
+
+# Evaluate on the test set
+print(f'\nMODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
---
+++ C1/week3/lab.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import models
+
+fmnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
+
+training_images, test_images = training_images / 255.0 , test_images / 255.0
+
+# Define the model
+model = tf.keras.models.Sequential([
+  # Add convolutions and max pooling
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+class myCallBack(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy') >= 0.9):
+      print("\nAccuracy is higher than 0.9 so stop training")
+      self.model.stop_training = True
+
+callbacks = myCallBack()
+
+# Print the model summary
+model.summary()
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+print(f'\nMODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
+
+# Evaluate on the test set
+print(f'\nMODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
+
+print(test_labels[:100])
+
+f, axarr = plt.subplots(3,4)
+
+FIRST_IMAGE=0
+SECOND_IMAGE=23
+THIRD_IMAGE=28
+CONVOLUTION_NUMBER = 1
+
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
+
+for x in range(0,4):
+  f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[0,x].grid(False)
+
+  f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[1,x].grid(False)
+
+  f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[2,x].grid(False)
